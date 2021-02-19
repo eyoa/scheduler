2123d373ba9d607fa6f4cc263002ad10dfaad99c
eyoa <yoaevelyn@gmail.com>
2021-02-18 21:12:06 -0500

    refactor to separate states and rendering

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,79 +1,21 @@
-import React, {useState, useEffect} from "react";
-import axios from 'axios';
+import React from "react";
 import "components/Application.scss";
 import DayList from 'components/DayList';
 import Appointment from 'components/Appointment/index';
 import {getAppointmentsForDay, getInterviewersForDay, getInterview} from 'helpers/selectors';
+import useApplicationData from '../hooks/useApplicationData'
 
 
 export default function Application(props) { 
-  const [state, setState] = useState({
-    day: "Monday",
-    days: [],
-    appointments: {},
-    interviewers: {}
-  });
-
-  useEffect(() => {
-    Promise.all([axios.get('/api/days'), axios.get('/api/appointments'), axios.get('api/interviewers')])
-    .then((response) => {
-      setState(prev => ({...prev, days: response[0].data, appointments: response[1].data, interviewers: response[2].data}))
-    })
-    
-  },[])
-
-  const setDay = function(day){
-    setState({...state, day});
-  }
-
+  const {
+    state,
+    setDay,
+    bookInterview,
+    cancelInterview
+  } = useApplicationData();
+  
   const dailyAppointments = getAppointmentsForDay(state, state.day);
   const interviewers = getInterviewersForDay(state, state.day);
-  
-
-
-  function bookInterview(id, interview) {
-    const appointment = {
-      ...state.appointments[id],
-      interview: {...interview}
-    }
-    const appointments = {
-      ...state.appointments,
-      [id]: appointment
-    };
-
-    return axios.put(`/api/appointments/${id}`, {interview})
-    .then(response => {
-      if(response.status === 204){
-        setState({...state, appointments})
-      }
-    })
-    .catch(e => {
-      console.log(e);
-    })
-      
-
-  }
-
-  function cancelInterview(id) {
-    
-    const appointment = {
-      ...state.appointments[id],
-      interview: null
-    };
-    const appointments = {
-      ...state.appointments,
-      [id]: appointment
-    };
-    return axios.delete(`/api/appointments/${id}`)
-    .then (response => {
-      console.log("got response", response);
-      if (response.status === 204){
-        setState({...state, appointments})
-      }
-    })
-    .catch(e => console.log(e)) 
-  }
-
 
 
   const schedule = dailyAppointments.map(appointment => {
--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -6,6 +6,7 @@ import Empty from './Empty'
 import Form from  './Form'
 import Status from './Status'
 import Confirm from './Confirm';
+import Error from './Error';
 import useVisualMode from '../../hooks/useVisualMode'
 
 // mode constants
@@ -16,6 +17,8 @@ const SAVING = "SAVING";
 const DELETING = "DELETING";
 const CONFIRM = "CONFIRM";
 const EDIT = "EDIT";
+const ERROR_SAVE = "ERROR_SAVE";
+const ERROR_DELETE = "ERROR_DELETE";
 
 
 export default function Appointment(props){
@@ -32,13 +35,14 @@ export default function Appointment(props){
     transition(SAVING);
     props.bookInterview(props.id, interview)
     .then(() => transition(SHOW))
-    
+    .catch(error => transition(ERROR_SAVE, true))
   }
 
   function deleteInterview() {
-    transition(DELETING);
+    transition(DELETING, true);
     props.cancelInterview(props.id)
     .then(() => transition(EMPTY))
+    .catch(error => transition(ERROR_DELETE, true))
   }
 
   return (
@@ -78,8 +82,18 @@ export default function Appointment(props){
           onSave ={save} 
         />
       )}
-      
-
+      {mode === ERROR_SAVE && (
+        <Error 
+          message="Could not save appointment."
+          onClose = {() => back()} 
+        />
+      )}
+      {mode === ERROR_DELETE && (
+        <Error 
+          message="Could not delete appointment." 
+          onClose = {() => back()} 
+        />
+      )}
 
     </article>
   )
