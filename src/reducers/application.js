032e7231df62f9efe9d2ade073965092204b6f3a
eyoa <yoaevelyn@gmail.com>
2021-02-24 23:13:49 -0500

    restore SET_DAY and SET_APP_DATA after checking circleci

--- src/reducers/application.js
+++ src/reducers/application.js
@@ -3,16 +3,16 @@ const SET_APP_DATA = 'SET_APP_DATA';
 const SET_INTERVIEW = 'SET_INTERVIEW';
 
 export default function reducer(state, action) {
-  // if (action.type === SET_DAY) {
-  //   const day = action.value;
-  //   return { ...state, day };
-  // }
-  // if (action.type === SET_APP_DATA) {
-  //   const days = [...action.days];
-  //   const appointments = action.appointments;
-  //   const interviewers = action.interviewers;
-  //   return { ...state, days, appointments, interviewers };
-  // }
+  if (action.type === SET_DAY) {
+    const day = action.value;
+    return { ...state, day };
+  }
+  if (action.type === SET_APP_DATA) {
+    const days = [...action.days];
+    const appointments = action.appointments;
+    const interviewers = action.interviewers;
+    return { ...state, days, appointments, interviewers };
+  }
 
   if (action.type === SET_INTERVIEW) {
     const days = action.days;
